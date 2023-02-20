3898249370abded7f58ce496f762bc0855f0580a
Shotray <695290109@qq.com>
2023-02-20 22:23:23 +0800

    Update process.py

--- code/process.py
+++ code/process.py
@@ -7,10 +7,13 @@ aminer_coauthor = '../data/AMiner-Coauthor/AMiner-Coauthor.txt'
 
 aminer_author_csv = '../data/AMiner-Author/AMiner-Author.csv'
 aminer_author_interest_csv = '../data/AMiner-Author/AMiner-Author-Interest.csv'
+aminer_author_affiliation_csv = '../data/AMiner-Author/AMiner-Author-Affiliation.csv'
 aminer_paper_csv = '../data/AMiner-Paper/AMiner-Paper.csv'
-aminer_author_affiliation_csv = '../data/AMiner-Paper/AMiner-Author-Affiliation.csv'
+# aminer_paper_author_affiliation_csv = '../data/AMiner-Paper/AMiner-Paper-Author-Affiliation.csv'
+aminer_paper_affiliation_csv = '../data/AMiner-Paper/AMiner-Paper-Affiliation.csv'
 aminer_paper_reference_csv = '../data/AMiner-Paper/Aminer-Paper-Reference.csv'
 aminer_coauthor_csv = '../data/AMiner-Coauthor/AMiner-Coauthor.csv'
+aminer_affiliation_csv = '../data/Aminer-Affiliation.csv'
 
 def process_author(read_file,write_file,affiliation_write_file,interest_write_file):
     f_read = open(read_file,'r',encoding='utf-8')
@@ -34,6 +37,7 @@ def process_author(read_file,write_file,affiliation_write_file,interest_write_fi
     count = 0
 
     for line in f_read:
+        line = line.replace("\"", " ").replace("\\", " ").replace("'", " ")
         count += 1
         if count % 100000 == 0:
                 print(count)
@@ -56,7 +60,7 @@ def process_author(read_file,write_file,affiliation_write_file,interest_write_fi
             # author_dict['t'] = str(per_author[8][3:-1])
             f_writer.writerow(author_dict)
             
-            affiliation_list = str(per_author[2])[3:-1].split(',')
+            affiliation_list = str(per_author[2])[3:-1].split(';')
             if affiliation_list!=['']:
                 for item in affiliation_list:
                     if item == '':
@@ -81,23 +85,25 @@ def process_author(read_file,write_file,affiliation_write_file,interest_write_fi
     f_affiliation_write.close()
     f_interest_write.close()
 
-def process_paper(read_file,write_file,paper_author_write_file):
+def process_paper(read_file,write_file,paper_affiliation_write_file):
     f_read = open(read_file,'r',encoding='utf-8')
     f_write = open(write_file,'w',encoding='utf-8',newline='')
-    f_paper_author_write = open(paper_author_write_file,'w',encoding='utf-8',newline='')
+    f_paper_affiliation_write = open(paper_affiliation_write_file,'w',encoding='utf-8',newline='')
 
     f_writer = csv.DictWriter(f_write,fieldnames=['index','paper_title','year','publication_venue','abstract'])
     f_writer.writeheader()
 
-    f_paper_author_writer = csv.DictWriter(f_paper_author_write,fieldnames=['paper_index','author_name','affiliation'])
-    f_paper_author_writer.writeheader()
+    f_paper_affiliation_writer = csv.DictWriter(f_paper_affiliation_write,fieldnames=['paper_index','affiliation'])
+    f_paper_affiliation_writer.writeheader()
 
     paper_dict = {'index':'','paper_title':'','year':'','publication_venue':'','abstract':''}
-    paper_author_affiliation_dict = {'paper_index':'','author_name':'','affiliation':''}
+    paper_affiliation_dict = {'paper_index':'','affiliation':''}
 
     count = 0
 
     for line in f_read:
+        line = line.replace("\"", " ").replace("\\", " ").replace("'", " ")
+
         count += 1
         if count % 100000 == 0:
             print(count) 
@@ -107,28 +113,29 @@ def process_paper(read_file,write_file,paper_author_write_file):
             if count > 1:
                 f_writer.writerow(paper_dict)
             paper_dict = paper_dict.fromkeys(['index','paper_title','year','publication_venue','abstract'],'')
-            paper_author_affiliation_dict = paper_author_affiliation_dict.fromkeys(['paper_index','author_name','affiliation'],'')
+            paper_affiliation_dict = paper_affiliation_dict.fromkeys(['paper_index','affiliation'],'')
             paper_dict['index'] = line[7:-1]
-            paper_author_affiliation_dict['paper_index'] = line[7:-1]
+            paper_affiliation_dict['paper_index'] = line[7:-1]
         elif line[:2] == '#*':
             paper_dict['paper_title'] = line[3:-1]
-        elif line[:2] == '#@':
-            paper_author_affiliation_dict['author_name'] = line[3:-1]
+        # elif line[:2] == '#@':
+        #     paper_affiliation_dict['author_name'] = line[3:-1]
         elif line[:2] == '#o':
-            paper_author_affiliation_dict['affiliation'] = line[3:-1]
-            author_name_list = paper_author_affiliation_dict['author_name'].split(';')
-            affiliation_list = paper_author_affiliation_dict['affiliation'].split(';')
-            if author_name_list == ['']:
+            paper_affiliation_dict['affiliation'] = line[3:-1]
+            affiliation_list = paper_affiliation_dict['affiliation'].split(';')
+            if affiliation_list == ['']:
                 continue
-            if len(affiliation_list) < len(author_name_list):
-                for i in range(len(author_name_list)-len(affiliation_list)):
-                    affiliation_list.append('-')
-            temp_dict = {'paper_index':'','author_name':'','affiliation':''}
-            for i in range(len(author_name_list)):
-                temp_dict['paper_index'] = paper_author_affiliation_dict['paper_index']
-                temp_dict['author_name'] = author_name_list[i]
+            temp_dict = {'paper_index':'','affiliation':''}
+            last_dict = {'paper_index':'','affiliation':''}
+            for i in range(len(affiliation_list)):
+                if affiliation_list[i] == '-':
+                    continue
+                temp_dict['paper_index'] = paper_affiliation_dict['paper_index']
                 temp_dict['affiliation'] = affiliation_list[i]
-                f_paper_author_writer.writerow(temp_dict)
+                if last_dict == temp_dict:
+                    continue
+                last_dict = temp_dict
+                f_paper_affiliation_writer.writerow(temp_dict)
         elif line[:2] == '#t':
             paper_dict['year'] = line[3:-1]
         elif line[:2] == '#c':
@@ -138,7 +145,7 @@ def process_paper(read_file,write_file,paper_author_write_file):
 
     f_read.close()
     f_write.close()
-    f_paper_author_write.close()
+    f_paper_affiliation_write.close()
 
 def process_paper_references(read_file,write_file):
     f_read = open(read_file,'r',encoding='utf-8')
@@ -183,8 +190,42 @@ def process_coauthor(read_file,write_file):
         coauthor_dict['co_num'] = coauthor_list[2]
         f_writer.writerow(coauthor_dict)
 
+def process_affiliation(read_file1,read_file2,write_file):
+    f_read1 = open(read_file1,'r',encoding='utf-8')
+    f_read2 = open(read_file2,'r',encoding='utf-8')
+    f_write = open(write_file,'w',encoding='utf-8',newline='')
+
+    reader1 = csv.DictReader(f_read1)
+    reader2 = csv.DictReader(f_read2)
+
+    f_writer = csv.DictWriter(f_write,fieldnames=['affiliation'])
+    f_writer.writeheader()
+
+    affiliation_dict = {'affiliation':''}
+
+    affiliation_set = set()
+    for line in reader1:
+        affiliation_set.add(line['affiliation'])
+
+    
+    for line in reader2:
+        affiliation_set.add(line['affiliation'])
+
+    for index, item in enumerate(affiliation_set):
+        if item == '':
+            continue
+        affiliation_dict['affiliation'] = item
+        f_writer.writerow(affiliation_dict)
+    
+    f_read1.close()
+    f_read2.close()
+    f_write.close()
+    
+  
+
 if __name__ == '__main__':
-    process_author(aminer_author,aminer_author_csv,aminer_author_affiliation_csv,aminer_author_interest_csv)
-    process_paper(aminer_paper,aminer_paper_csv,aminer_author_affiliation_csv)
-    process_paper_references(aminer_paper,aminer_paper_reference_csv)
-    process_coauthor(aminer_coauthor,aminer_coauthor_csv)
+    # process_author(aminer_author,aminer_author_csv,aminer_author_affiliation_csv,aminer_author_interest_csv)
+    # process_paper(aminer_paper,aminer_paper_csv,aminer_paper_affiliation_csv)
+    # process_paper_references(aminer_paper,aminer_paper_reference_csv)
+    # process_coauthor(aminer_coauthor,aminer_coauthor_csv)
+    process_affiliation(aminer_author_affiliation_csv,aminer_paper_affiliation_csv,aminer_affiliation_csv)
